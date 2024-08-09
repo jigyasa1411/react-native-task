debb94a79c91abffc2e21db76ecd8ce411fa516c
vandana sharma <vandanasharma@Vandana-Sharma.local>
2024-08-09 18:32:40 +0530

    update

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {enableScreens} from 'react-native-screens';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {Book} from './model/Book';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { enableScreens } from 'react-native-screens';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Book } from './model/Book';
 import BookDetailScreen from './screens/BookDetailScreen';
 import BookListScreen from './screens/BookListScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import RootStackParamList from './model/RootStackParamList';
 import TestScreen from './screens/TestScreen';
 
+
 enableScreens();
+
+
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
